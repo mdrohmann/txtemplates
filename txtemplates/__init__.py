b346b15d3eb465ec828a31fea0a00df3ae582942
Martin Drohmann <mdrohmann@gmail.com>
2015-04-21 09:00:41 -0700

    bump version number

--- txtemplates/__init__.py
+++ txtemplates/__init__.py
@@ -5,6 +5,6 @@ This is the main module of the txtemplates project.
 from dist import Version
 
 
-version = Version('txtemplates', 0, 4, 0, 'alpha')
+version = Version('txtemplates', 0, 4, 0)
 
 #  vim: set spell spelllang=en sw=4:
