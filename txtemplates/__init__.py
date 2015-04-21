ee09d6fd7b8489aeb70d6dcddf9aa2363fb72dbe
Martin Drohmann <mdrohmann@gmail.com>
2015-04-21 08:53:47 -0700

    [dist] use version numbers

--- txtemplates/__init__.py
+++ txtemplates/__init__.py
@@ -1,6 +1,10 @@
 """
-This is the main module of the |project| project.
-
+This is the main module of the txtemplates project.
 """
+
+from dist import Version
+
+
+version = Version('txtemplates', 0, 4, 0, 'alpha')
 
 #  vim: set spell spelllang=en sw=4:
